a16a53c3a5619243570152a32194523f1c1afe47
mesbah <116296813+mesbahuddin11@users.noreply.github.com>
2022-11-09 11:49:50 -0500

    Update ITEM_MASTER FILE.sql

--- ITEM_MASTER FILE.sql
+++ ITEM_MASTER FILE.sql
@@ -1,5 +1,7 @@
-select sim.Prod as  'Product No',
-       tmp.SEQ_NUM, 
+
+
+select     sim.Prod as  'Product No',
+           tmp.SEQ_NUM, 
 	   tmp.ITEM_ID,
 	   tmp.DESCRIPTION, 
 	   tmp.OPT_INFO_2 as 'Package_Type', 
@@ -12,10 +14,14 @@ select sim.Prod as  'Product No',
 	   tmp.WGT_UNIT,
 	   tmp.OPT_INFO_3 as 'UOM',
 	   tmp.OPT_INFO_4 as 'Conv_Factor',
-       sim.[Qty on Hand],   
-       sim.[Usage Rate] as 'Usage per Month',
+           sim.[Qty on Hand],   
+           sim.[Usage Rate] as 'Usage per Month',
 	   sim.[Curr Usage] as 'Usage of Cur Month',
-       fp.location_id
+           fp.location_id,
+	   sim.Prodcat,
+	   sim.[PC Buyer],
+	   fp.replen_level,
+	   fp.replen_qty
 	    
 from sx_item_master sim
 join (select *, ROW_NUMBER () over 
